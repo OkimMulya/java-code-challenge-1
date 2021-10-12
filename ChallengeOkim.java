b7f2cb463e5b487820f84c0f4bf84fdf218e92bd
OkimMulya <okim.mulya@gmail.com>
2021-10-12 17:31:53 +0700

    Challenge1

---
+++ ChallengeOkim.java
@@ -0,0 +1,31 @@
+public class ChallengeOkim {
+        public static void main(String[] args) {
+            // Membuat nama alamat dari peserta
+          // first name , lastname, age, phone number, address, and other
+          //System.out.println("Hello World");
+          
+          // Inisialisasi
+          String first_name = "";
+          String last_name = "";
+          long phone_number;
+          String address = "";
+          int age = 0;
+          
+          // Input variabel
+          first_name = "Okim";
+          last_name = "Mulya";
+          phone_number = 1234567;
+          address = "Bogor";
+          age = 27;
+          
+          
+          // Print in to screen
+          System.out.println("=======================================");
+          System.out.println("||      First name   = " + first_name );
+          System.out.println("||      Last name    = " + last_name);
+          System.out.println("||      Phone number = " + phone_number);
+          System.out.println("||      Address      = " + address);
+          System.out.println("||      Age          = " + age);
+          System.out.println("=======================================");
+        }
+    }
